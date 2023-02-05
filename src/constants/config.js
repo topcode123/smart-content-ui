827046d4d3730a0efb0edd313002310516fce441
Cuong <cuong.cxview.ai>
2023-02-05 16:46:27 +0000

    update login

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,4 +3,5 @@ export const localeOptions = [
     { id: 'en', name: 'English', icon: 'flag-icon-us' },
     { id: 'es', name: 'Español', icon: 'flag-icon-es' },
 ];
-export const baseURL = 'http://94.237.76.12:8090';
+// export const baseURL = 'http://94.237.76.12:8090';
+export const baseURL = 'https://smartcontent.momd.vn';
